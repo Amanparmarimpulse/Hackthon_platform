85ca0765aaf224ece8e8695a7286fcdbf1a6b072
Aman parmar <amanparmar0715@gmail.com>
2024-06-23 11:47:42 +0530

    Update index.js

--- Hackthon_platform-aman_backendcode/backend/index.js
+++ Hackthon_platform-aman_backendcode/backend/index.js
@@ -2,38 +2,43 @@ const express = require("express");
 const mongoose = require('mongoose');
 const cors = require("cors");
 const DeveloperModel = require("./models/developer_profiledata");
-const admin = require('firebase-admin');
 const serviceAccount = require('./firebasekey/serviceAccountKey.json');
+const admin = require('firebase-admin');
 require('dotenv').config();
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT;
-const HOST = process.env.HOST;
+const PORT = process.env.PORT || 3001;
 
+// Initialize Firebase Admin SDK
 // Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-mongoose.connect(`${process.env.MONGO_URL}`);
-
-app.post('/developer', (req, res) => {
-     DeveloperModel.create(req.body)
-     .then(developerProfile => res.json(developerProfile))
-     .catch(err => res.json(err));
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB', err);
 });
 
-
+app.post('/developer', (req, res) => {
+  DeveloperModel.create(req.body)
+    .then(developerProfile => res.json(developerProfile))
+    .catch(err => res.json(err));
+});
 
 app.get('/developers', (req, res) => {
-    DeveloperModel.find()
-      .then(developerProfiles => res.json(developerProfiles))
-      .catch(err => res.json(err));
-  });
+  DeveloperModel.find()
+    .then(developerProfiles => res.json(developerProfiles))
+    .catch(err => res.json(err));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running at ${HOST}:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
