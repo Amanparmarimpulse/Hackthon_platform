5057598c8e8ce532cf5fd3739c875a6e781c36b1
Aman parmar <amanparmar0715@gmail.com>
2024-06-23 12:42:20 +0530

    Update index.js

--- Hackthon_platform-aman_backendcode/backend/index.js
+++ Hackthon_platform-aman_backendcode/backend/index.js
@@ -1,1 +1,39 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const cors = require("cors");
+const DeveloperModel = require("./models/developer_profiledata");
+const admin = require('firebase-admin');
+const serviceAccount = require('./firebasekey/serviceAccountKey.json');
+require('dotenv').config();
+const app = express();
 
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+mongoose.connect(`${process.env.MONGO_URL}`);
+
+app.post('/developer', (req, res) => {
+     DeveloperModel.create(req.body)
+     .then(developerProfile => res.json(developerProfile))
+     .catch(err => res.json(err));
+});
+
+
+
+app.get('/developers', (req, res) => {
+    DeveloperModel.find()
+      .then(developerProfiles => res.json(developerProfiles))
+      .catch(err => res.json(err));
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server is running at ${HOST}:${PORT}`);
+});
